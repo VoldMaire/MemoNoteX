70c1658bfeed7c2cc872a49592c551fa8be1afb1
VoldMaire <levenvov@gmail.com>
2015-10-11 22:34:00 +0300

    changeScript

--- changeVersion.ps1
+++ changeVersion.ps1
@@ -1,14 +1,14 @@
 $VerFromAssemblyInfo = get-content SharedAssemblyInfo.cs
 $InstallScript = get-content Install\installscriptMemoNoteUnicode.iss
-$VerFromAssemblyInfo[27][30]
-$i = 31
+$VerFromAssemblyInfo[26][27]
+$i = 27
 $verString = ""
 do
 	{
-	$verString = $verString + $VerFromAssemblyInfo[27][$i]
+	$verString = $verString + $VerFromAssemblyInfo[26][$i]
 	$i = $i + 1
 	}
-while($VerFromAssemblyInfo[27][$i] -ne ')')
+while($VerFromAssemblyInfo[26][$i] -ne ')')
 $verString
 $InstallScript[2]
 $InstallScript[2] = "#define Version " + $verString
